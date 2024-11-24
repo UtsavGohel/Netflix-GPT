91751b638a0b667f20c8b1b17072c6de5918c802
UtsavGohel <utsavgohel2002@gmail.com>
2024-11-24 15:57:06 +0530

    feat(redux-slice): implemented add-trailer-video slice in redux store

--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -7,13 +7,14 @@ const moviesSlice = createSlice({
   },
   reducers: {
     addNowPlayingMovies: (state, action) => {
-      console.log("action.payload", action.payload);
-
       state.nowPlayingMovies = action.payload;
+    },
+    addTrailerVideo: (state, action) => {
+      state.trailerVideo = action.payload;
     },
   },
 });
 
-export const { addNowPlayingMovies } = moviesSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo } = moviesSlice.actions;
 
 export default moviesSlice.reducer;
