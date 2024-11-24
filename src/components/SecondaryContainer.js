a7b1196273657f7d220e4a9ff18213c7fa38128a
UtsavGohel <utsavgohel2002@gmail.com>
2024-11-24 21:18:23 +0530

    feat(movie-list): implement custom movie list in secondary container

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,7 +1,24 @@
 import React from "react";
+import MovieList from "./MovieList";
+
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer</div>;
+  const movies = useSelector((store) => store.movies);
+
+  return (
+    <div className=" bg-black">
+      <div className="-mt-52 pl-12 relative z-20">
+        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Popular"} movies={movies.popularMoviesVideo} />
+        <MovieList
+          title={"Trending Playing"}
+          movies={movies.nowPlayingMovies}
+        />
+        <MovieList title={"Now Trending"} movies={movies.nowPlayingMovies} />
+      </div>
+    </div>
+  );
 };
 
 export default SecondaryContainer;
