40a67cd83370f9dcdb48df6e123ee075ca94b94b
UtsavGohel <utsavgohel2002@gmail.com>
2024-11-24 15:59:16 +0530

    feat(now-playing-movies): add custom hook to fetch now playing movies from API

---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+import { API_OPTIONS } from "../utils/const";
+
+export const useNowPlayingMovies = () => {
+  //Fetch data from TMDB API and update store
+  const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+
+    dispatch(addNowPlayingMovies(json.results));
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+};
