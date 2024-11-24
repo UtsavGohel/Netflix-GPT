867e290d45300e26a17e9fd41f4eb26c69446d2d
UtsavGohel <utsavgohel2002@gmail.com>
2024-11-24 21:11:59 +0530

    feat(popular-movie-hook): implement custom hook to retrive popular movies

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/const";
+import { useDispatch } from "react-redux";
+import { addPopularMoviesVideo } from "../utils/movieSlice";
+
+const usePopularMovies = () => {
+  const dispatch = useDispatch();
+
+  const getPopularMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+      API_OPTIONS
+    );
+
+    const json = await data.json();
+
+    dispatch(addPopularMoviesVideo(json.results));
+  };
+
+  useEffect(() => {
+    getPopularMovies();
+  }, []);
+};
+
+export default usePopularMovies;
