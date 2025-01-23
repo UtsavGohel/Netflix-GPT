a54a2631f6dea2097e445103fb3aa9f0eb2e2451
UtsavGohel <utsavgohel2002@gmail.com>
2025-01-23 19:48:23 +0530

    feat(language): add supported language constant

--- src/utils/const.js
+++ src/utils/const.js
@@ -16,3 +16,18 @@ export const API_OPTIONS = {
 };
 
 export const TMDB_IMAGE_CDN_URL = "https://image.tmdb.org/t/p/w500/";
+
+export const SUPPORTED_LANGUAGE = [
+  {
+    identifier: "en",
+    name: "english",
+  },
+  {
+    identifier: "hindi",
+    name: "hindi",
+  },
+  {
+    identifier: "spanish",
+    name: "spanish",
+  },
+];
